585d060df80448dbf9b5fa5c6643c6f66a1a77d1
blackbatmanfan <93589270+blackbatmanfan@users.noreply.github.com>
2022-05-31 17:11:56 +0530

    Add files via upload

--- lets_chat.js
+++ lets_chat.js
@@ -6,3 +6,15 @@ function adduser()
 
     window.location = "lets-chat_room.html";
 }
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAgcJ8bmALJAHKI7iPIT7vy-dbRNDm7bg8",
+    authDomain: "kwitter-ripoff-46298.firebaseapp.com",
+    databaseURL: "https://kwitter-ripoff-46298-default-rtdb.firebaseio.com",
+    projectId: "kwitter-ripoff-46298",
+    storageBucket: "kwitter-ripoff-46298.appspot.com",
+    messagingSenderId: "18547805049",
+    appId: "1:18547805049:web:f94cf100f6803d568f7da8"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
---
+++ lets_chat_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAgcJ8bmALJAHKI7iPIT7vy-dbRNDm7bg8",
+    authDomain: "kwitter-ripoff-46298.firebaseapp.com",
+    databaseURL: "https://kwitter-ripoff-46298-default-rtdb.firebaseio.com",
+    projectId: "kwitter-ripoff-46298",
+    storageBucket: "kwitter-ripoff-46298.appspot.com",
+    messagingSenderId: "18547805049",
+    appId: "1:18547805049:web:f94cf100f6803d568f7da8"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
